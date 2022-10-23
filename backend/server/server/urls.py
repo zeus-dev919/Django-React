907e5649bfe60957a3ac4a16d00d1d37e335f863
BestTopPro <ccccodechef@gmail.com>
2022-10-22 21:12:05 -0500

    add backend authentication

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.accounts.urls import accounts_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += accounts_urlpatterns # add URLs for authentication
